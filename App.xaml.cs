983aff07920a2a7a991e21f42e2f006372fbd550
Leon Osenger <leon@caron.ch>
2023-03-15 19:07:52 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Beschleunigungssensor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Beschleunigungssensor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
